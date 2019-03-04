89860c2bb8ab6c1b44d0c3cf52f1d495bc9e1415
Shilpi <bhattacharya.shilpi@yahoo.com>
2019-03-04 22:50:11 +0530

    dfdssfdfdf

---
+++ WebApiDemo/WebApiDemo/Controllers/EmployeesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataAccess;
+
+namespace WebApiDemo.Controllers
+{
+    [Serializable]
+    public class EmployeesController : ApiController
+    {     
+        
+        public IEnumerable<Employee> GetEmployees()
+        {
+            using (ShilpiExamplesEntities entities = new ShilpiExamplesEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+
+        public Employee GetEmployees(int id)
+        {
+            using (ShilpiExamplesEntities entities = new ShilpiExamplesEntities())
+            {
+                return entities.Employees.Where(x => x.ID == id).First();
+            }
+        }
+    }
+}
